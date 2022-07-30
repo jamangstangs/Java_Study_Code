43ceceb2f0ff069d72f9cda3d16284fbecfe0b14
jamangstangs <jamangsong@gmail.com>
2022-07-30 23:06:36 +0900

    Java 8 이전, 무게별로 필터링하는 메서드를 구현했다.

--- src/java8/methodReference/Apple.java
+++ src/java8/methodReference/Apple.java
@@ -3,6 +3,16 @@ package java8.methodReference;
 public class Apple {
 
     private Color color;
+    private int weight;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public Apple(Color color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
 
     public Apple(Color color) {
         this.color = color;
--- src/java8/methodReference/MethodReferenceMain.java
+++ src/java8/methodReference/MethodReferenceMain.java
@@ -10,14 +10,16 @@ public class MethodReferenceMain {
     public static void main(String[] args) {
         // 사과 리스트 생성
         List<Apple> inventory = new ArrayList<>();
-        inventory.add(new Apple(RED));
-        inventory.add(new Apple(GREEN));
-        inventory.add(new Apple(PURPLE));
-        inventory.add(new Apple(RED));
-        inventory.add(new Apple(GREEN));
-        inventory.add(new Apple(RED));
+        inventory.add(new Apple(RED,100));
+        inventory.add(new Apple(GREEN,100));
+        inventory.add(new Apple(PURPLE,200));
+        inventory.add(new Apple(RED,100));
+        inventory.add(new Apple(GREEN,100));
+        inventory.add(new Apple(RED,100));
 
         System.out.println(filterGreenApples(inventory));
+        System.out.println(filterHeavyApples(inventory));
+
     }
 
     public static List<Apple> filterGreenApples(List<Apple> inventory) {
@@ -25,6 +27,18 @@ public class MethodReferenceMain {
 
         for (Apple apple : inventory) {
             if (GREEN.equals(apple.getColor())) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static List<Apple> filterHeavyApples(List<Apple> inventory) {
+        // 150 이상 필터링한다고 하자.
+        List<Apple> result = new ArrayList<>();
+
+        for (Apple apple : inventory) {
+            if (apple.getWeight() > 150 ) {
                 result.add(apple);
             }
         }
