09b1cfa084ee51a9bb9971803fe206fce8a75bb8
jamangstangs <jamangsong@gmail.com>
2022-08-08 15:58:02 +0900

    Consumer 함수형 인터페이스를 ㄹ마다로 전달해 단순히 출력하는 방식으로 구현

---
+++ src/java8/lambda/ConsumerMain.java
@@ -0,0 +1,23 @@
+package java8.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerMain {
+
+    public static void main(String[] args) {
+
+        forEach(
+                Arrays.asList(1, 2, 3, 4, 5),
+                (Integer i) -> System.out.println("i = " + i)
+        );
+
+    }
+
+    public static <T> void forEach(List<T> list, Consumer<T> c) {
+        for (T t : list) {
+            c.accept(t);
+        }
+    }
+}
